5823ae990783b758e27c6f6d4c7db4d417a7fd22
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-04-20 21:45:04 -0500

    js eventos

---
+++ Mercadeo/js/main.js
@@ -0,0 +1,28 @@
+let cerrar = document.querySelectorAll(".close");
+
+cerrar.forEach(function (close) {
+  close.addEventListener("click", function (ev) {
+    // ev.preventDefault(); con este evento prevenimos que se ejecute
+    ev.preventDefault();
+
+    let content = document.querySelector(".content");
+
+    //Quitarle las clases de animacion que ya tiene
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+
+    //Agregar clases para animar su saludo fadeOutUp
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+    //setTimeout
+    // setInterval(() => {
+
+    // }, interval);
+    setTimeout(function () {
+      location.href = "/Mercadeo";
+    }, 600);
+
+    return false;
+  });
+});
